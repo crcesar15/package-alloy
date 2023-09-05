ea725f8ec3f1a532c9eee7760efa0fdf9baefa17
Cesar Rodriguez <crcesar15@gmail.com>
2023-09-05 10:23:56 -0400

    Remove unnecessary files

--- routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => ['auth']], function () {
-    Route::get('admin/package-alloy', [PackageAlloyController::class, 'index'])->name('package.skeleton.index');
-    Route::get('package-alloy', [PackageAlloyController::class, 'index'])->name('package.skeleton.tab.index');
-});
